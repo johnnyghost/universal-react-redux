abf56d896c6eed33521312d4f739868d49ef048c
johnnyghost <kemarx@gmail.com>
2016-11-29 14:03:46 +0000

    Add production configuration.

--- configs/webpack/webpack.development.babel.js
+++ configs/webpack/webpack.development.babel.js
@@ -1,41 +1,30 @@
 const webpack           = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const config            = require('./webpack.common.babel.js');
+const commonConfig            = require('./webpack.common.babel.js');
 
-config.entry = config.entry.concat([
-  'webpack-dev-server/client?http://localhost:3000/',
-  'webpack/hot/only-dev-server'
-]);
+const devConfig = {
+  entry: commonConfig.entry.concat([
+    'webpack-dev-server/client?http://localhost:3000/',
+    'webpack/hot/only-dev-server'
+  ]),
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: 'dist',
+    inline: true,
+    port: 3000,
+    colors: true,
+    historyApiFallback: {
+      index: '/dist/'
+    }
+  },
+  plugins: commonConfig.plugins.concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'public/index.html'
+    })
+  ])
+}
 
-config.devtool = 'cheap-module-eval-source-map';
-
-config.devServer = {
-  contentBase: 'dist',
-  inline: true,
-  port: 3000,
-  colors: true,
-  historyApiFallback: {
-    index: '/dist/'
-  }
-};
-
-config.plugins = config.plugins.concat([
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin(),
-  new HtmlWebpackPlugin({
-    inject: true,
-    template: 'public/index.html'
-  })
-]);
-
-// config.module = Object.assign({}, config.module, {
-//   preLoaders: [
-//     {
-//       test: /\.jsx?$/,
-//       loaders: ['babel-loader', 'eslint'],
-//       exclude: /node_modules/
-//     }
-//   ]
-// });
-
-module.exports = config;
+module.exports = Object.assign(commonConfig, devConfig);
---
+++ configs/webpack/webpack.production.babel.js
@@ -0,0 +1,55 @@
+const webpack           = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig            = require('./webpack.common.babel.js');
+
+const prodConfig = {
+  output: Object.assign(commonConfig.output, {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].chunk.js',
+  }),
+  devtool: 'source-map',
+  plugins: commonConfig.plugins.concat([
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      children: true,
+      minChunks: 2,
+      async: true,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      },
+      compress: {
+        warnings: false,
+        screw_ie8: true
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      isProduction: true,
+      template: 'public/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      }
+    })
+  ])
+}
+
+
+module.exports = Object.assign(commonConfig, prodConfig);
