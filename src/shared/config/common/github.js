e12b42a2bf35e9379a7a5b79247e4e0e6dce758e
johnnyghost <kemarx@gmail.com>
2017-01-04 10:32:56 +0000

    (fix) dev token.

--- src/shared/config/common/github.js
+++ src/shared/config/common/github.js
@@ -1,7 +1,10 @@
-import { GITHUB_TOKEN } from './tokens';
-
 const config = {
-  TOKEN: process.env.GITHUB_TOKEN || GITHUB_TOKEN
+  TOKEN: process.env.GITHUB_TOKEN
 };
+
+if(__DEV__) {
+  const { GITHUB_TOKEN } = require('./tokens');
+  config.TOKEN = GITHUB_TOKEN;
+}
 
 export default config;
